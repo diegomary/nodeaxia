366b10682ec8caf37348998e190eee8be2a5cb4f
Diego Aldo Burlando <diegoaldoburlando62@gmail.com>
2023-05-17 11:38:52 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import * as path from 'path'
 app.use('/', express.static(path.join(__dirname, 'static')));
 
 app.get('/api/hello', (req, res) => {
-  res.send('This is an api used to register users')
+  res.send('This is an api used to register users......................................')
 })
 
 app.listen(port, () => {
