0302d521310cec285d21ed1fd4a2636fc2145a0b
Diego Aldo Burlando <diegoaldoburlando62@gmail.com>
2023-05-17 11:06:27 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 80
 
-app.use(express.static('static'))
+//app.use(express.static('static'))
 
 app.get('/api/hello', (req, res) => {
   res.send('This is an api used to register users')
