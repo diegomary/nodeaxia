867e4ab21975949fba5c3fe690051b6d8c1b3bc8
Diego Aldo Burlando <diegoaldoburlando62@gmail.com>
2023-05-17 11:31:16 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express')
+import express from 'express'; 
 const app = express()
 const port = 80
-const path = require("path");
+import path from "path"
 
 app.use('/', express.static(path.join(__dirname, 'static')));
 
